b4cb01519c6cead60413f0c22f1757a34d109328
Kifoo <magdaa2622@gmail.com>
2024-05-15 10:44:29 +0200

    Correct utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,27 @@
-
-def add ( a: int , b: int) -> int:
+"""
+File contains functions that add, sybstract, multiply and divide two integers.
+"""
+def add(a: int, b: int) -> int:
+    """
+    Add two integers.
+    """
     return a + b
-def subtract ( a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """
+    Substract two integers.
+    """
     return a - b
-def multiply ( a: int , b: int) -> int:
+
+
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply two integers.
+    """
     return a * b
-def divide ( a: int , b: int) -> float :
+
+def divide(a: int, b: int) -> float:
+    """
+    Divide two integers.
+    """
     return a / b
